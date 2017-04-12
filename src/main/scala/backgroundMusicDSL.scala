e7129cedb9cd961b51673c863559bd48857cb498
George Reason <george@reasonfamily.co.uk>
2017-04-11 19:15:16 -0500

    Added DSL and runner

--- src/main/scala/backgroundMusicDSL.scala
+++ src/main/scala/backgroundMusicDSL.scala
@@ -2,8 +2,6 @@
   * Created by george.reason on 4/11/17.
   */
 
-object HelloWorld {
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
+object backgroundMusicDSL {
+
 }
---
+++ src/main/scala/backgroundMusicRunner.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by george.reason on 4/11/17.
+  */
+
+//import backgroundMusicDSL
+
+object backgroundMusicRunner {
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
